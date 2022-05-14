fa4a87275328b78b10f941a05f3cdde88eb675aa
CarolineDuffett <ckduffett@gmail.com>
2022-05-14 18:40:55 -0400

    made controllers directory

---
+++ controllers/routes.js
@@ -0,0 +1,90 @@
+const express = require('express')
+const router = express.Router()
+const fishSeed = require('../models/seed.js') //seed data
+const Fish = require('../models/schema.js') //schema
+
+
+
+//___________________
+// Routes
+//___________________
+//---Test Route
+// //localhost:3000
+// router.get('/' , (req, res) => {
+//   res.send('Hello World!');
+// });
+
+//--- SEED ROUTE
+router.get('/seed', (req, res) => {
+  Fish.create(fishSeed, (err, data) => {
+    res.redirect('/')
+  })
+})
+
+//--- INDEX ROUTE
+router.get('/', (req, res) => {
+  Fish.find({}, (err, allFish) => {
+    res.render('index.ejs',
+      {
+        fishData: allFish,
+      }
+    )
+  })
+})
+
+//--- Create ROUTE
+router.post('/', (req, res) => {
+  if (req.body.img === '') {
+    req.body.img = 'https://i.imgur.com/EXotp4G.png';
+  }
+  Fish.create(req.body, (err, createdFish) => {
+    Fish.findByIdAndUpdate({_id: req.params.id}, {diet: req.body.diet.split(",")}, {new: true}, (err, createdFish) => {
+      res.redirect('/')
+    })
+  })
+})
+
+//--- NEW ROUTE
+router.get('/new', (req, res) => {
+  res.render('new.ejs')
+})
+
+//--- UPDATE ROUTE
+router.put('/:id', (req, res) => {
+  Fish.findByIdAndUpdate({_id: req.params.id}, req.body, {new: true}, (err, updatedFish) => {
+    Fish.findByIdAndUpdate({_id: req.params.id}, {diet: req.body.diet.split(',')}, {new: true}, (err, updatedDish) => {
+      res.redirect('/')
+    })
+  })
+})
+
+//--- SHOW ROUTE
+router.get('/:id', (req, res) => {
+  Fish.findById(req.params.id, (err, foundFish) => {
+    res.render('show.ejs',
+      {
+        fishData: foundFish,
+      }
+    )
+  })
+})
+
+//--- Edit ROUTE
+router.get('/:id/edit', (req, res) => {
+  Fish.findById(req.params.id, (err, foundFish) => {
+    res.render('edit.ejs',
+      {
+        fishData: foundFish,
+      }
+    )
+  })
+})
+
+//--- DESTROY (DELETE) ROUTE
+router.delete('/:id', (req, res) => {
+  Fish.deleteOne({_id: req.params.id}, (err, deletedFish) => {
+    res.redirect('/')
+  })
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 //___________________
 //Dependencies
 //___________________
-const express = require('express');
-const methodOverride  = require('method-override');
-const mongoose = require ('mongoose');
-const app = express ();
-const db = mongoose.connection;
+const express = require('express')
+const methodOverride  = require('method-override')
+const controller = require('./controllers/routes.js'); //not in original version
+const mongoose = require ('mongoose')
+const app = express ()
+const db = mongoose.connection
 require('dotenv').config()
 
 
@@ -20,28 +21,28 @@ const PORT = process.env.PORT
 //Database
 //___________________
 // How to connect to the database either via heroku or locally
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI
 
 
 // Connect to Mongo &
 // Fix Depreciation Warnings from Mongoose
 // May or may not need these depending on your Mongoose version
 mongoose.connect(MONGODB_URI , () => {
-  console.log('connected to mongo');
+  console.log('connected to mongo')
 });
 
 
 // Error / success
-db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
-db.on('disconnected', () => console.log('mongo disconnected'));
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'))
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI))
+db.on('disconnected', () => console.log('mongo disconnected'))
 
 
 //___________________
 //Models
 //___________________
-const fishSeed = require('./models/seed.js'); //seed data
-const Fish = require('./models/schema.js'); //schema
+//const fishSeed = require('./models/seed.js') //seed data (original)
+//const Fish = require('./models/schema.js') //schema (original)
 
 
 //___________________
@@ -49,99 +50,100 @@ const Fish = require('./models/schema.js'); //schema
 //___________________
 
 //use public folder for static assets
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // populates req.body with parsed info from forms - if no data from forms will return an empty object {}
-app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
+app.use(express.urlencoded({ extended: false }))// extended: false - does not allow nested objects in query strings
 app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
 
 //use method override
-app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
+app.use(methodOverride('_method'))// allow POST, PUT and DELETE from a form
+
+app.use('/', controller) //not in original
 
 
 
-
-//___________________
-// Routes
-//___________________
-//---Test Route
-// //localhost:3000
-// app.get('/' , (req, res) => {
-//   res.send('Hello World!');
-// });
-
-//--- SEED ROUTE
-app.get('/seed', (req, res) => {
-  Fish.create(fishSeed, (err, data) => {
-    res.redirect('/')
-  })
-})
-
-//--- INDEX ROUTE
-app.get('/', (req, res) => {
-  Fish.find({}, (err, allFish) => {
-    res.render('index.ejs',
-      {
-        fishData: allFish,
-      }
-    )
-  })
-})
-
-//--- Create ROUTE
-app.post('/', (req, res) => {
-  if (req.body.img === '') {
-    req.body.img = 'https://i.imgur.com/EXotp4G.png';
-  }
-  Fish.create(req.body, (err, createdFish) => {
-    Fish.findByIdAndUpdate({_id: req.params.id}, {diet: req.body.diet.split(",")}, {new: true}, (err, createdFish) => {
-      res.redirect('/')
-    })
-  })
-})
-
-//--- NEW ROUTE
-app.get('/new', (req, res) => {
-  res.render('new.ejs')
-})
-
-//--- UPDATE ROUTE
-app.put('/:id', (req, res) => {
-  Fish.findByIdAndUpdate({_id: req.params.id}, req.body, {new: true}, (err, updatedFish) => {
-    Fish.findByIdAndUpdate({_id: req.params.id}, {diet: req.body.diet.split(',')}, {new: true}, (err, updatedDish) => {
-      res.redirect('/')
-    })
-  })
-})
-
-//--- SHOW ROUTE
-app.get('/:id', (req, res) => {
-  Fish.findById(req.params.id, (err, foundFish) => {
-    res.render('show.ejs',
-      {
-        fishData: foundFish,
-      }
-    )
-  })
-})
-
-//--- Edit ROUTE
-app.get('/:id/edit', (req, res) => {
-  Fish.findById(req.params.id, (err, foundFish) => {
-    res.render('edit.ejs',
-      {
-        fishData: foundFish,
-      }
-    )
-  })
-})
-
-//--- DESTROY (DELETE) ROUTE
-app.delete('/:id', (req, res) => {
-  Fish.deleteOne({_id: req.params.id}, (err, deletedFish) => {
-    res.redirect('/')
-  })
-})
+// //___________________
+// // Routes
+// //___________________
+// //---Test Route
+// // //localhost:3000
+// // app.get('/' , (req, res) => {
+// //   res.send('Hello World!');
+// // });
+//
+// //--- SEED ROUTE
+// app.get('/seed', (req, res) => {
+//   Fish.create(fishSeed, (err, data) => {
+//     res.redirect('/')
+//   })
+// })
+//
+// //--- INDEX ROUTE
+// app.get('/', (req, res) => {
+//   Fish.find({}, (err, allFish) => {
+//     res.render('index.ejs',
+//       {
+//         fishData: allFish,
+//       }
+//     )
+//   })
+// })
+//
+// //--- Create ROUTE
+// app.post('/', (req, res) => {
+//   if (req.body.img === '') {
+//     req.body.img = 'https://i.imgur.com/EXotp4G.png';
+//   }
+//   Fish.create(req.body, (err, createdFish) => {
+//     Fish.findByIdAndUpdate({_id: req.params.id}, {diet: req.body.diet.split(",")}, {new: true}, (err, createdFish) => {
+//       res.redirect('/')
+//     })
+//   })
+// })
+//
+// //--- NEW ROUTE
+// app.get('/new', (req, res) => {
+//   res.render('new.ejs')
+// })
+//
+// //--- UPDATE ROUTE
+// app.put('/:id', (req, res) => {
+//   Fish.findByIdAndUpdate({_id: req.params.id}, req.body, {new: true}, (err, updatedFish) => {
+//     Fish.findByIdAndUpdate({_id: req.params.id}, {diet: req.body.diet.split(',')}, {new: true}, (err, updatedDish) => {
+//       res.redirect('/')
+//     })
+//   })
+// })
+//
+// //--- SHOW ROUTE
+// app.get('/:id', (req, res) => {
+//   Fish.findById(req.params.id, (err, foundFish) => {
+//     res.render('show.ejs',
+//       {
+//         fishData: foundFish,
+//       }
+//     )
+//   })
+// })
+//
+// //--- Edit ROUTE
+// app.get('/:id/edit', (req, res) => {
+//   Fish.findById(req.params.id, (err, foundFish) => {
+//     res.render('edit.ejs',
+//       {
+//         fishData: foundFish,
+//       }
+//     )
+//   })
+// })
+//
+// //--- DESTROY (DELETE) ROUTE
+// app.delete('/:id', (req, res) => {
+//   Fish.deleteOne({_id: req.params.id}, (err, deletedFish) => {
+//     res.redirect('/')
+//   })
+// })
 
 
 
