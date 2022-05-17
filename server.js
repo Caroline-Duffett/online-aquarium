01a1cdd26a7bb24e05d862642c7be50dfa014c8f
CarolineDuffett <ckduffett@gmail.com>
2022-05-16 21:06:43 -0400

    added jQuery and jsdom back in for showpage dropdown menu

--- server.js
+++ server.js
@@ -42,6 +42,16 @@ db.on('disconnected', () => console.log('mongo disconnected'))
 
 
 //___________________
+//For jQuery to work
+//___________________
+let jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+const { document } = (new JSDOM('')).window;
+global.document = document;
+let $ = require("jquery")(window);
+
+//___________________
 //Middleware
 //___________________
 
@@ -86,39 +96,6 @@ app.listen(PORT, () => console.log( 'Listening on port:', PORT));
 
 
 //---------- Graveyard ----------//
-//--- Original edit form:
-// <form class="fish-form" action="/<%=fishData._id%>?_method=PUT" method="POST">
-//   Name: <input type="text" name="name" value="<%=fishData.name%>" required/><br/>
-//   Scientific Name: <input type="scientificName" name="scientificName" value="<%=fishData.scientificName%>"/><br/>
-//   Description: <input type="text" name="description" value="<%=fishData.description%>" required/><br/>
-//   Animal Type: <input type="text" name="animalType" value="<%=fishData.animalType%>" required/><br/>
-//   Range: <input type="text" name="range" value="<%=fishData.range%>" required/><br/>
-//   Image URL: <input type="url" name="img" value="<%=fishData.img%>"/><br/>
-//   Number: <input type="number" name="number" value="<%=fishData.number%>" required/><br/>
-//   Diet: <input type="text" name="diet" value="<%=fishData.diet%>"/><br/>
-//   Size: <input type="text" name="size" value="<%=fishData.size%>"/><br/>
-//
-//   <input class='form-button' type="submit" value="Submit Changes"/>
-// </form>
-
-//--- Original finsh-wrapper before index flexbox
-// .fish-wrapper {
-//   margin: 2em auto;
-//   width: 50%;
-//   display: flex;
-//   flex-direction: column;
-//   flex-wrap: nowrap;
-//   background-color: lightblue;
-//   border-radius: 2em;
-//   align-items: center;
-// }
-
-//original background
-// background-color: #ebf5fb;
-// background-image: url("data:image/svg+xml,%3Csvg width='100' height='20' viewBox='0 0 100 20' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M21.184 20c.357-.13.72-.264 1.088-.402l1.768-.661C33.64 15.347 39.647 14 50 14c10.271 0 15.362 1.222 24.629 4.928.955.383 1.869.74 2.75 1.072h6.225c-2.51-.73-5.139-1.691-8.233-2.928C65.888 13.278 60.562 12 50 12c-10.626 0-16.855 1.397-26.66 5.063l-1.767.662c-2.475.923-4.66 1.674-6.724 2.275h6.335zm0-20C13.258 2.892 8.077 4 0 4V2c5.744 0 9.951-.574 14.85-2h6.334zM77.38 0C85.239 2.966 90.502 4 100 4V2c-6.842 0-11.386-.542-16.396-2h-6.225zM0 14c8.44 0 13.718-1.21 22.272-4.402l1.768-.661C33.64 5.347 39.647 4 50 4c10.271 0 15.362 1.222 24.629 4.928C84.112 12.722 89.438 14 100 14v-2c-10.271 0-15.362-1.222-24.629-4.928C65.888 3.278 60.562 2 50 2 39.374 2 33.145 3.397 23.34 7.063l-1.767.662C13.223 10.84 8.163 12 0 12v2z' fill='%235dade2' fill-opacity='0.21' fill-rule='evenodd'/%3E%3C/svg%3E");
-//color: #85C1E9
-
-
 //kinda working post route:
 // app.post('/', (req, res) => {
 //   if (req.body.img === '') {
@@ -130,112 +107,6 @@ app.listen(PORT, () => console.log( 'Listening on port:', PORT));
 //     })
 //   })
 // })
-
-
-//original show page styling:
-// #commonName {
-//   margin: 0;
-// }
-//
-// #scienceName {
-//   font-style: italic;
-//   margin: 0;
-//   margin-bottom: 1%;
-// }
-//
-// .fish-info-wrapper {
-//   margin: 2em auto;
-//   width: 70%;
-//   min-width: 400px;
-//   display: flex;
-//   flex-direction: column;
-//   flex-wrap: nowrap;
-//   background-color: #ebf5fb;
-//   border-radius: 2em;
-//   padding: 1.5em;
-//   justify-content: space-between;
-//   align-items: center;
-//   border: solid;
-//   border-color: gray;
-//   border-width: thin;
-// }
-//
-// .fish-info-list {
-//   display: flex;
-//   flex-direction: row;
-//   flex-wrap: wrap;
-//   justify-content: space-evenly;
-//   background-color: tan;
-// }
-//
-// .fish-name-con {
-//   text-align: right;
-// }
-//
-// .img-description {
-//   width: 70%;
-//   margin-top: 2%;
-//   margin-left: auto;
-//   margin-right: auto;
-//   background-color: yellow;
-// }
-//
-// .fish-img-con {
-//   max-width: 100%;
-//   min-width: 300px;
-//
-// }
-//
-// .fish-img-con img {
-//   max-height: 100%;
-//   max-width: 100%;
-//   border-radius: 1em;
-// }
-//
-// .right-side {
-//   width: 25%;
-//   align-items: center;
-//   min-width: 200px;
-//   background-color: pink;
-// }
-//
-// .animalType-con {
-//   margin-left: 5%;
-// }
-//
-// .fish-quantity-con {
-//   margin-left: 5%;
-// }
-//
-// .fish-size-con {
-//   margin-left: 5%;
-// }
-//
-// .fish-range-con {
-//   margin-left: 5%;
-// }
-//
-// .fish-diet-con {
-//   margin-left: 5%;
-// }
-//
-// /*
-// .bottom-info-block {
-//   display: flex;
-//   flex-direction: row;
-//   flex-wrap: nowrap;
-//   justify-content: space-evenly;
-//   align-items: center;
-// }
-// */
-//
-// .fish-description-con {
-//   margin: 0 2%;
-// }
-//
-// .show-page-nav {
-//
-// }
 
 
 
@@ -272,89 +143,6 @@ app.listen(PORT, () => console.log( 'Listening on port:', PORT));
 //     "mongoose": "^6.3.3"
 //   }
 // }
-
-
-
-//jquery dropdown styling:
-// .hamburger-lines-div {
-//   background-color: pink;
-//   position: fixed;
-//   align-self: flex-end;
-// }
-//
-// .hamburger-lines {
-//  background-color: black;
-//  padding: 12px;
-//  display: flex;
-//  height: 26px;
-//  flex-direction: column;
-//  justify-content: space-between;
-//  width: 30px;
-// }
-//
-// .line {
-//   height: 4px;
-//   width: 30px;
-//   border-radius: 10px;
-//   background: white;
-// }
-//
-// .drop-menu {
-//   display: flex;
-//   flex-direction: column;
-//   align-self: flex-end;
-// }
-
-//jquerydropdown ejs:
-// <div class="hamburger-lines-div">
-//   <div id='open' class="hamburger-lines">
-//     <span class="line"></span>
-//     <span class="line"></span>
-//     <span class="line"></span>
-//   </div>
-//   <div id='close' class="hamburger-lines">
-//     <span class="line"></span>
-//     <span class="line"></span>
-//     <span class="line"></span>
-//   </div>
-// </div>
-// <div id="drop" class="drop-menu">
-//   <a href="/<%=fishData._id%>/edit"/> <button type="button" name="button">Edit</button></a>
-//   <form action='/<%=fishData._id%>?_method=DELETE' method='POST'>
-//     <input type="submit" value="Delete">
-//   </form>
-// </div>
-// <script>
-//   $(() => {
-//
-//     $('#close').hide()
-//     $('.drop-menu').hide()
-//
-//     $('#open').click( () => {
-//       $('#open').hide()
-//       $('#close').show()
-//       $('.drop-menu').show()
-//     })
-//
-//     $('#close').click( () => {
-//       $('#close').hide()
-//       $('#open').show()
-//       $('.drop-menu').hide()
-//     })
-//   });
-// </script>
-
-// //___________________
-// //For jQuery to work
-// //___________________
-// let jsdom = require("jsdom");
-// const { JSDOM } = jsdom;
-// const { window } = new JSDOM();
-// const { document } = (new JSDOM('')).window;
-// global.document = document;
-// let $ = require("jquery")(window);
-
-//in head.ejs: <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 
 
 
@@ -466,3 +254,24 @@ app.listen(PORT, () => console.log( 'Listening on port:', PORT));
 //   position: absolute;
 // }
 // */
+
+
+// //Nav without jQuery:
+// <nav class='show-page-bottom-nav'>
+//   <a id="edit-link" href="/<%=fishData._id%>/edit"/> <button id="edit-btn"  type="button" name="button">Edit</button></a>
+//   <form id="form-link" action='/<%=fishData._id%>?_method=DELETE' method='POST'>
+//     <input id="delete-btn" type="submit" value="Delete">
+//   </form>
+// </nav>
+
+// //css:
+// .show-page-bottom-nav {
+//   display: flex;
+//   flex-wrap: nowrap;
+//   flex-direction: row;
+//   justify-content: space-evenly;
+//   margin-top: 3%;
+//   margin-left: auto;
+//   margin-right: auto;
+//   width: 20%;
+// }
